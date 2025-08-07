b3ee53350da8a73f846ee815d9dc0a7d939a6a74
SahithiAekka <sahithiaekka0261@gmail.com>
2025-08-07 13:15:07 -0400

    flask app for Ci/CD

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from your Flask app deployed on AWS EC2!"
+    return render_template("index.html")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
 
-# http://3.143.242.208:5000/ -- 3.143.242.208 is the public IP of the EC2 instance 
+
+# http://127.0.0.1:5000 only works from inside the EC2 instance itself (not from your browser).
+# http://18.219.18.219:5000 is the correct way to access it publicly (from your laptop, phone, etc.).
+
